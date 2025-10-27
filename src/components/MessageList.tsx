d02fc44ae6b6ee743fe46e5ad6de617146b3a995
decevy <laurens@decevy.com>
2025-10-27 22:51:31 +0100

    Enhance MessageList to display message timestamps and usernames; refactor message rendering for improved layout and styling.

--- src/components/MessageList.tsx
+++ src/components/MessageList.tsx
@@ -3,6 +3,7 @@
 import { useChat } from '../contexts/ChatContext';
 import { useAuth } from '../contexts/AuthContext';
 import { useEffect, useRef } from 'react';
+import { format } from 'date-fns';
 
 export function MessageList() {
   const { messages, lastMessage } = useChat();
@@ -20,11 +21,15 @@ export function MessageList() {
   
   return (
     <div ref={messageListRef} className="mx-6 py-3 flex flex-col flex-1 gap-1 overflow-y-auto no-scrollbar">
-      {messages.map((message) => (
-        <div key={message.id} className={`py-1 px-4 rounded-lg  ${message.user.id === user?.id ? 'self-end bg-blue-200' : 'self-start bg-gray-200'}`}>
-          <p>{message.content}</p>
-        </div>
-      ))}
+      {messages.map((message) => {
+        const isOwnMessage = message.user.id === user?.id;
+        return (
+          <div key={message.id} className={`${isOwnMessage ? 'self-end' : 'self-start'}`}>
+            <p className={`px-1.5 text-xs text-gray-500 ${isOwnMessage ? 'text-right' : 'text-left'}`}>{isOwnMessage ? '' : `${message.user.username} `} {format(new Date(message.createdAt), 'HH:mm')}</p>
+            <p className={`py-1 px-4 rounded-lg  ${isOwnMessage ? 'bg-blue-200' : 'bg-gray-200'}`}>{message.content}</p>
+          </div>
+        );
+      })}
     </div>
   );
 }
